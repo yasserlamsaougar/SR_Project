487f0c28a98787fe8dbb9de29de231ce66bedc29
Yasser <yasser.lamsougar@gmail.com>
2014-01-20 05:05:59 +0100

    Added BackUp server Address

--- src/shared/CONSTANTS.java
+++ src/shared/CONSTANTS.java
@@ -5,5 +5,7 @@ public class CONSTANTS {
 	public final static String ServiceName = "TestService";
 	public final static int servicePort = 10000;
 	
+	public final static String b_ServiceName = "backupService";
+	public final static int b_servicePort = 10001;
 	
 }
