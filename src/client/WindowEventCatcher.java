ed14a6423c300e623e5259b872928170b98118e2
Yasser <yasser.lamsougar@gmail.com>
2014-01-20 05:01:57 +0100

    Taking into Account Crashes

--- src/client/WindowEventCatcher.java
+++ src/client/WindowEventCatcher.java
@@ -2,16 +2,22 @@ package client;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 import server.ServerService;
+import shared.CONSTANTS;
 
 public class WindowEventCatcher extends WindowAdapter {
 
 	private GameScreen g;
 	private ServerService service;
-	public WindowEventCatcher(GameScreen g, ServerService service) {
+	private GameManager gameManager;
+	public WindowEventCatcher(GameScreen g, GameManager gameManager ,ServerService service) {
 		this.g = g;
+		this.gameManager = gameManager;
 		this.service = service;
 	}
 
@@ -21,8 +27,16 @@ public class WindowEventCatcher extends WindowAdapter {
 			System.out.println("disconnecting");
 			service.disconnect(g.getOwnId());
 		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			String serverObjectName = "rmi://localhost:" + CONSTANTS.b_servicePort
+					+ "/" + CONSTANTS.b_ServiceName;
+			try {
+				Registry registry = LocateRegistry.getRegistry(null, CONSTANTS.b_servicePort);
+				service = (ServerService) registry.lookup(serverObjectName);
+				service.reconnect(gameManager, g.getOwnId());
+				service.disconnect(g.getOwnId());
+			} catch (RemoteException | NotBoundException exception) {
+				
+			}
 		}
 	}
 
